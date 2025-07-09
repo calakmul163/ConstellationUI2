0c7bd5c7a73165c58d0136f457bb7fe15ecb3ef3
Calakmul163 <calakmul163@gmail.com>
2025-07-09 11:25:23 -0600

    Update microsite.js

---
+++ config/microsite.js
@@ -0,0 +1,14 @@
+/* 
+AUTHOR: ROBERTO SOSA CIGARROA
+COPYRIGHT: behance.net/rsosa
+*/
+document.addEventListener('alpine:init', () => {
+    Alpine.data('app', () => ({
+        micrositio: {
+            logofooter: {
+                image: "https://res.cloudinary.com/dmem56hib/image/upload/v1734116724/ns6avom2p9eiuak42knr",
+                alt: "Secretaría de la Mujer e Igualdad de Género",
+            },
+        }
+    }));
+});

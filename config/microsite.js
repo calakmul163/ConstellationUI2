16eb0c80ccbe3ec2a8edfa637560ea950873ba44
Calakmul163 <calakmul163@gmail.com>
2025-07-09 12:39:47 -0600

    Update microsite.js

--- config/microsite.js
+++ config/microsite.js
@@ -4,11 +4,34 @@ COPYRIGHT: behance.net/rsosa
 */
 document.addEventListener('alpine:init', () => {
     Alpine.data('app', () => ({
+        footerData: {},
+        isLoading: true,
+        error: null,
+        
         micrositio: {
             logofooter: {
                 image: "https://res.cloudinary.com/dmem56hib/image/upload/v1734116724/ns6avom2p9eiuak42knr",
                 alt: "Secretaría de la Mujer e Igualdad de Género",
             },
+        },
+        
+        async init() {
+            try {
+                const response = await fetch('/api/v1/footer');
+                if (!response.ok) throw new Error('Error al cargar datos del footer');
+                
+                const data = await response.json();
+                if (data.success) {
+                    this.footerData = data.data;
+                } else {
+                    throw new Error(data.message || 'Error en los datos del footer');
+                }
+            } catch (err) {
+                this.error = err.message;
+                console.error('Error:', err);
+            } finally {
+                this.isLoading = false;
+            }
         }
     }));
 });

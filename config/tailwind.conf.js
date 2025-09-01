e37114dbd9d8e7e975849c484544ff67881a9061
Calakmul163 <calakmul163@gmail.com>
2025-09-01 12:11:06 -0600

    Update tailwind config

--- config/tailwind.conf.js
+++ config/tailwind.conf.js
@@ -2,97 +2,88 @@
 AUTHOR: ROBERTO SOSA CIGARROA
 COPYRIGHT: behance.net/rsosa
 */
-tailwind.config = {
+// config/tailwind.config.js
+export default {
     theme: {
-        extend: {
-            colors: {
-                bs: {
-                    teal: {
-                        50: '#E5F5F3',
-                        100: '#CCEBE7',
-                        200: '#B2E1DB',
-                        300: '#99D7CF',
-                        400: '#80CDC3',
-                        500: '#66C3B7',
-                        600: '#4DB9AB',
-                        650: '#00BEB0',
-                        700: '#33AF9F',
-                        800: '#1AA593',
-                        900: '#009C8B',
-                        950: '#10766B'
-                    },
-                    pink: {
-                        50: '#FBE6EF',
-                        100: '#F8CDE0',
-                        200: '#F4B4D0',
-                        300: '#F19BC0',
-                        400: '#ED82B0',
-                        500: '#EA69A1',
-                        600: '#E65091',
-                        700: '#E33781',
-                        800: '#DF1E71',
-                        900: '#DC0562',
-                        950: '#C91160'
-                    },
-                    red: {
-                        50: '#F7E9EA',
-                        100: '#EFD2D4',
-                        200: '#E8BCBF',
-                        300: '#E0A5AA',
-                        400: '#D88F94',
-                        500: '#D1797F',
-                        600: '#C9626A',
-                        700: '#C14C54',
-                        800: '#B9363F',
-                        900: '#B9363F',
-                        950: '#87252b'
-                    },
-                    black: {
-                        50: '#E9E9E9',
-                        100: '#D3D2D2',
-                        200: '#BDBCBC',
-                        300: '#A7A5A6',
-                        400: '#918F90',
-                        500: '#7B7979',
-                        600: '#656263',
-                        700: '#504C4D',
-                        800: '#3A3636',
-                        900: '#241F20',
-                        950: '#1B1A1A'
-                    },
-                    beige: {
-                        50: '#FBF9F7',
-                        100: '#F7F3EF',
-                        200: '#F3EDE8',
-                        300: '#EFE7E0',
-                        400: '#EBE0D8',
-                        500: '#E7DAD1',
-                        600: '#E3D4C9',
-                        700: '#DFCEC1',
-                        800: '#DBC8B9',
-                        900: '#D7C2B2',
-                        950: '#BFAC59',
-                        1000: '#AA9950'
-                    }
+        colors: {
+            bs: {
+                teal: {
+                    50: '#E5F5F3',
+                    100: '#CCEBE7',
+                    200: '#B2E1DB',
+                    300: '#99D7CF',
+                    400: '#80CDC3',
+                    500: '#66C3B7',
+                    600: '#4DB9AB',
+                    650: '#00BEB0',
+                    700: '#33AF9F',
+                    800: '#1AA593',
+                    900: '#009C8B',
+                    950: '#10766B',
                 },
-                pm: {
-                    violet: {
-                        500: '#7060A8'
-                    },
-                    purple: {
-                        500: '#8D60A6'
-                    },
-                    pink: {
-                        500: '#DC0562'
-                    },
-                    amber: {
-                        500: '#F2A039'
-                    },
-                    lime: {
-                        500: '#7BC255'
-                    }
-                }
+                pink: {
+                    50: '#FBE6EF',
+                    100: '#F8CDE0',
+                    200: '#F4B4D0',
+                    300: '#F19BC0',
+                    400: '#ED82B0',
+                    500: '#EA69A1',
+                    600: '#E65091',
+                    700: '#E33781',
+                    800: '#DF1E71',
+                    900: '#DC0562',
+                    950: '#C91160',
+                },
+                red: {
+                    50: '#F7E9EA',
+                    100: '#EFD2D4',
+                    200: '#E8BCBF',
+                    300: '#E0A5AA',
+                    400: '#D88F94',
+                    500: '#D1797F',
+                    600: '#C9626A',
+                    700: '#C14C54',
+                    800: '#B9363F',
+                    900: '#B9363F',
+                    950: '#87252b',
+                },
+                black: {
+                    50: '#E9E9E9',
+                    100: '#D3D2D2',
+                    200: '#BDBCBC',
+                    300: '#A7A5A6',
+                    400: '#918F90',
+                    500: '#7B7979',
+                    600: '#656263',
+                    700: '#504C4D',
+                    800: '#3A3636',
+                    900: '#241F20',
+                    950: '#1B1A1A',
+                },
+                beige: {
+                    50: '#FBF9F7',
+                    100: '#F7F3EF',
+                    200: '#F3EDE8',
+                    300: '#EFE7E0',
+                    400: '#EBE0D8',
+                    500: '#E7DAD1',
+                    600: '#E3D4C9',
+                    700: '#DFCEC1',
+                    800: '#DBC8B9',
+                    900: '#D7C2B2',
+                    950: '#BFAC59',
+                    1000: '#AA9950',
+                },
             },
+            pm: {
+                violet: { 500: '#7060A8' },
+                purple: { 500: '#8D60A6' },
+                pink: { 500: '#DC0562' },
+                amber: { 500: '#F2A039' },
+                lime: { 500: '#7BC255' },
+            },
+        },
+        extend: {
             backgroundImage: {
                 'im-herogob': "url('https://res.cloudinary.com/dmem56hib/image/upload/v1741981188/S2CCAcU6ybhSIKff')",
                 'im-banner-lineadigital': "url('https://res.cloudinary.com/dmem56hib/image/upload/v1742332631/nryOdrDOlK6kFxCz')",
@@ -105,8 +96,8 @@ tailwind.config = {
                 'pa-footer-row1-teal': "url('https://res.cloudinary.com/dmem56hib/image/upload/v1743714152/f7yerrjhput3tfj04fqy')",
                 'pa-footer-row1-beige': "url('https://res.cloudinary.com/dmem56hib/image/upload/v1742836800/kca2sjqudr7tp58rratc')",
                 'pa-footer-row1-micrositios': "url('https://res.cloudinary.com/dmem56hib/image/upload/v1752772245/BxOEMAyVZeN6FFXU')",
-                'mi-gradiente': 'linear-gradient(to right, #FF6347, #4682B4)'
-            }
-        }
-    }
+                'mi-gradiente': 'linear-gradient(to right, #FF6347, #4682B4)',
+            },
+        },
+    },
 }
